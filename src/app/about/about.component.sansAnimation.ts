cdff869ce55990a686e7c302c86a1237fe834698
salem53 <salem.dhouimir@ensi-uma.tn>
2021-05-15 22:35:39 +0100

    update acceuil

--- src/app/about/about.component.sansAnimation.ts
+++ src/app/about/about.component.sansAnimation.ts
@@ -17,12 +17,6 @@ export class AboutComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (!localStorage.getItem('firstReload') || localStorage.getItem('firstReload') === 'true') {
-      localStorage.setItem('firstReload', 'false');
-      window.location.reload();
-    } else {
-      localStorage.setItem('firstReload', 'true');
-    }
 
     }
   /*reloadComponent() {
